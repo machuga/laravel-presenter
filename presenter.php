8e21a312694f14a060a2b6bb5bf94b3425f7c54f
Matthew Machuga <machuga@gmail.com>
2012-08-08 23:30:27 -0400

    Remove reference to default, remove reference to entry, remove 5.4 syntax

--- presenter.php
+++ presenter.php
@@ -4,13 +4,10 @@ class Presenter
 {
 	public $resource = null;
 	public $resource_name = 'resource';
-	public $default = 'resource';
 
 	public function __construct($resource = null)
 	{
-		if ($resource) {
-			$this->resource = $resource;
-		}
+		$resource and $this->resource = $resource;
 
         // Alias the resource name for nicer, more meaninful access
         // Example: $this->entry, $this->post, $this->user
@@ -22,14 +19,14 @@ class Presenter
 		if (method_exists($this, $key)) {
 			return $this->{$key}();
 		} else {
-			return $this->resource->$key ?: $this->default;
+			return $this->resource->$key;
 		}
 	}
 
 	public function __call($key, $args)
 	{
 		if (method_exists($this->resource, $key)) {
-			return call_user_func_array([$this->resource, $key], $args);
+			return call_user_func_array(array($this->resource, $key), $args);
 		} else {
 			throw new Exception('Presenter: '.get_called_class().'::'.$key.' method does not exist');
 		}
@@ -37,6 +34,6 @@ class Presenter
 
     public function __toString()
     {
-        return $this->entry->__toString();
+        return $this->resource->__toString();
     }
 }
